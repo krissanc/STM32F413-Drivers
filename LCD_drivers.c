389c65ca0691f4c4ea941d9791a4e8a355c4a7ae
Kristian Curkovic <30583095+krissanc@users.noreply.github.com>
2022-06-22 15:36:34 +0200

    Add files via upload

--- LCD_drivers.c
+++ LCD_drivers.c
@@ -15,15 +15,13 @@
 
 
 /*** LCD Initialization Function ***/
-// example: LCD_Init(PA4, PA5, PA6, PC5, PC6, PC7, PC8)
-// - so 1st parameter initializes desired MCU pin to the connected RS pin, and so forth
+// Example: LCD_Init(&LCD)
+// It will fetch the values stored in the object, just pass its address
 
 void LCD_Pin_Init(LCD_PINOUT_t *pLCD)
 {
 	// take the pins that are connected to the MCU: (#1 RS), (#2 Read/Write), (#3 Enable), (#4 D4), (#5 D5), (#6 D6), (#7 D7)
 	// and initialize them
-
-
 
 /********** RS config ***********/
 
@@ -84,6 +82,59 @@ void LCD_Pin_Init(LCD_PINOUT_t *pLCD)
 } /*** End of LCD Initialization function ***/
 
 
+void LCD_Set4bits(LCD_PINOUT_t *pLCD, uint8_t data)
+{
+	// Let's say value of data is  0 0 1 1 (0x3)   then D5 will be  0 0 0 1   after the shift, ANDed with 1, and be set to HIGH
+
+	/* D4 */ GPIOx_Pin_Write((GPIOx_REG_DEF_t*)pLCD->LCD_D4.port, pLCD->LCD_D4.pin, ((data >> 0) & 0x1));
+	/* D5 */ GPIOx_Pin_Write((GPIOx_REG_DEF_t*)pLCD->LCD_D5.port, pLCD->LCD_D5.pin, ((data >> 1) & 0x1));
+	/* D6 */ GPIOx_Pin_Write((GPIOx_REG_DEF_t*)pLCD->LCD_D6.port, pLCD->LCD_D6.pin, ((data >> 2) & 0x1));
+	/* D7 */ GPIOx_Pin_Write((GPIOx_REG_DEF_t*)pLCD->LCD_D7.port, pLCD->LCD_D7.pin, ((data >> 3) & 0x1));
+
+}
+
+void LCD_Send_Command(LCD_PINOUT_t *pLCD, uint8_t data)
+{
+
+	// Sending 4 bits at a time (Higher nibble first)
+	// Set Register Select (RS) to LOW
+	/* RS */ GPIOx_Pin_Write((GPIOx_REG_DEF_t*)pLCD->LCD_RS.port, pLCD->LCD_RS.pin, LOW);
+
+	// Set Read / NOT Write to 0 to Write
+	/* RW */ GPIOx_Pin_Write((GPIOx_REG_DEF_t*)pLCD->LCD_RW.port, pLCD->LCD_RW.pin, LOW);
+
+	// set Enable to HIGH
+	/* EN */ GPIOx_Pin_Write((GPIOx_REG_DEF_t*)pLCD->LCD_EN.port, pLCD->LCD_EN.pin, HIGH);
+
+	// The LCD reads what is on the data lines at the EN transition from HIGH to LOW
+	// Set D4-D7
+	LCD_Set4bits(pLCD, (data >> 4));
+
+	// Set EN to LOW to read what is on the lines
+	/* EN */ GPIOx_Pin_Write((GPIOx_REG_DEF_t*)pLCD->LCD_EN.port, pLCD->LCD_EN.pin, LOW);
+
+	// Set EN to HIGH
+	/* EN */ GPIOx_Pin_Write((GPIOx_REG_DEF_t*)pLCD->LCD_EN.port, pLCD->LCD_EN.pin, HIGH);
+
+	// Set D4-D7
+	LCD_Set4bits(pLCD, data);
+
+	// Set EN to LOW to read what is on the lines
+	/* EN */ GPIOx_Pin_Write((GPIOx_REG_DEF_t*)pLCD->LCD_EN.port, pLCD->LCD_EN.pin, LOW);
+
+
+}
+
+void LCD_Display_Init(LCD_PINOUT_t *pLCD)
+{
+
+
+
+
+
+}
+
+
 void LCD_SetCursor(LCD_PINOUT_t *pLCD)
 {
 
@@ -91,10 +142,5 @@ void LCD_SetCursor(LCD_PINOUT_t *pLCD)
 
 }
 
-void LCD_Write(uint8_t data)
-{
 
-
-
-}
 
--- LCD_drivers.h
+++ LCD_drivers.h
@@ -37,7 +37,14 @@ typedef struct
 // Function Prototypes
 
 void LCD_Pin_Init(LCD_PINOUT_t *pLCD);
-void LCD_Write(uint8_t data);
+
+void LCD_Display_Init(LCD_PINOUT_t *pLCD);
+
+void LCD_Set4bits(LCD_PINOUT_t *pLCD, uint8_t data);
+
+void LCD_Send_Command(LCD_PINOUT_t *pLCD, uint8_t data);
+
+
 
 
 
